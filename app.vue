4303041bd22f50e9ccf4ea7fc509a09ebb9688b8
alizeba <zeba.ali350@gmail.com>
2018-08-11 21:45:02 +0530

    app.vue

---
+++ app.vue
@@ -0,0 +1,20 @@
+<html>
+<body>
+<html>
+<div id = "app"></div>
+</html>
+<script>
+const vue = new Vue({
+  el : '#app',
+  data() {
+    return {
+     title: 'Its Simple Title'
+   }
+  }
+ 
+})
+</script>
+</body>
+</html>
+
+
